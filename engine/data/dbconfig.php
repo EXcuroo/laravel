6d7902129df5051acbf5d26faf6b073dbe68c7c5
EXcuroo <32517765+EXcuroo@users.noreply.github.com>
2018-01-06 19:57:30 +0600

    first commit

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "kino");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8");
+
+define('SECURE_AUTH_KEY', 'z87,/#P/k~)Oy3u}zL%8c[Fj#(i|7kp89n{~g(.@8<[AY(|,m#8gPzb{g8;OE=');
+
+$db = new db;
+
+?>
